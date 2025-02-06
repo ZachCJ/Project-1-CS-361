de8015f0aa2734c6f661b731a1a316ee2ff905de
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2025-02-06 12:11:30 -0700

    Made changes so that two DFAStates can be compared properly

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -26,4 +26,23 @@ public class DFAState extends State {
     public Map<Character, DFAState> getTransitions() {
         return transitions;
     }
+
+    // The following methods allow us to use contains() on a LinkedHashSet<> to
+    // check if a State is in the set or not
+    // Takes in and Object which is cast to DFAState then checks if its name matches
+    // the current States name
+    @Override
+    public boolean equals(Object newState) {
+        // Cast newState to be a DFAState
+        DFAState other = (DFAState) newState;
+        return this.getName().equals(other.getName());
+    }
+
+    // Allows us to compare based on name
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
+
+    // TODO Add a toString()?
 }
