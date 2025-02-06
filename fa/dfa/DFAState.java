241b708d510e224fadd276b2b118689cffd1fa28
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2025-02-06 11:06:22 -0700

    Started on DFAState.java

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,7 +1,29 @@
 package fa.dfa;
 
+import java.util.Map;
+
 import fa.State;
 
 public class DFAState extends State {
+    // to store transitions for this state
+    Map<Character, DFAState> transitions;
 
+    public DFAState(String name) {
+        super(name);
+    }
+
+    // Add a transition for this state
+    public void addTransition(char symbol, DFAState nextState) {
+        transitions.put(symbol, nextState);
+    }
+
+    // Get the next state for a given input symbol
+    public DFAState getNextState(char symbol) {
+        return transitions.get(symbol);
+    }
+
+    // Get all transitions for this state
+    public Map<Character, DFAState> getTransitions() {
+        return transitions;
+    }
 }
